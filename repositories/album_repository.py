fb0688105616908f0e03a53e5d4687b41dc54dcf
Nargis <nargis.salim@btinternet.com>
2020-11-24 16:42:23 +0000

    added album repository

--- repositories/album_repository.py
+++ repositories/album_repository.py
@@ -4,5 +4,11 @@ from db.run_sql import run_sql
 
 def save(album):
     sql = "INSERT INTO albums (artist_id, title, genre) VALUES (%s, %s, %s) RETURNING id"
+    values = [album.artist.id, album.title, album.genre]
+    results = run_sql(sql, values)
+    id = results[0]["id"]
+    album.id = id
+    return album
+
     
 
